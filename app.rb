1658ab07a941e56714239fcb96de824df54ed0a8
John Dickerson <TheSpook@gmail.com>
2013-10-14 00:26:18 -0400

    Linking
    Added auto-linking to bib files.

--- app.rb
+++ app.rb
@@ -5,14 +5,22 @@ configure do
 
 end
 
+# List of research projects
 get '/research/?' do  
   haml :research
 end
 
+# List of all publications
 get '/publications/?' do
   haml :publications
 end
 
+# Eventually, auto-generate individual pages per publication
+get '/publication/:title' do
+  haml :publications
+end
+
+# Home page (links to all the other main pages)
 get '/' do
   haml :index
 end
--- helpers/bib_helpers.rb
+++ helpers/bib_helpers.rb
@@ -2,21 +2,27 @@ helpers do
 
   # Takes an array of string keys into a .bib file and returns an HTML
   # list of their citations, formatted via cite_to_link
-  def rep_pubs_list(bib_keys)
+  def rep_pubs_list(bib_keys, include_links=false)
     out = "<ul class='list-unstyled'>"
     
     out += bib_keys.uniq.map do |bib_key| 
       "<li><span class='glyphicon glyphicon-chevron-right'></span>
-#{cite_to_link($bib[bib_key])}
-</li>"
+#{cite_to_link($bib[bib_key])} " +
+        ((include_links == true) ? "[<a href=\"#{bib_key_to_link(bib_key)}\">link</a>]" : "") + 
+        "</li>"
     end.join
-
+    
     out += "</ul>"
   end
 
 
   def cite_to_link(bib_struct)
     CiteProc.process bib_struct.to_citeproc, :style => $bib_citestyle
+  end
+
+  # Translate a bib_key that looks like "NameYear:Title" into a URL
+  def bib_key_to_link(bib_key)
+    bib_key.downcase.delete(':')
   end
 
 end
