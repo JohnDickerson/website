e5df8fdbaffe40d3ac0525dc8678e8c21d70e26b
John Dickerson <TheSpook@gmail.com>
2013-10-23 15:17:45 -0400

    Details details details.
    Filled out almost all of the index, made a side projects page.

--- app.rb
+++ app.rb
@@ -6,18 +6,26 @@ configure do
 end
 
 # List of research projects
-get '/research/?' do  
+get '/research/?' do
+  @subtitle = "Research"
   haml :research
 end
 
 # List of all publications
 get '/pubs/?' do
+  @subtitle = "Publications"
   haml :publications
 end
 
 # Grab a {.pdf, .pptx} for a specific publication
 get '/pubs/:title' do
   #send_file File.join(settings.public_folder, params[:title]+'.pdf')
+end
+
+# Lists side projects (similar to research page)
+get '/side_projects/?' do
+  @subtitle = "Side Projects"
+  haml :side_projects
 end
 
 # Home page (links to all the other main pages)
